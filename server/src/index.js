87892a00167ed6d15b395dd5dc021db6a9db0f73
rutanshubhayani <rutanshubhayani4252@gmail.com>
2025-10-03 23:32:50 +0530

    local host changed

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,8 @@ import jwt from 'jsonwebtoken';
 
 const app = express();
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://decomind.vercel.app'],
+  // origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://decomind.vercel.app'],
+  origin: "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
